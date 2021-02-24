11135e003a343ee70c989bebb68d689addd13aa7
Tiyas <tiyas.akbar@gmail.com>
2021-02-25 03:28:07 +0700

    ready deploy"

---
+++ seeders/20210224202340-author.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert("Authors", [
+			{
+				id: 1,
+				name: "Tiyas",
+				email: "tiyas@gmail.com",
+				password: "$2b$10$7hbN2PKBlaHUyovbm0fvL.GCG/y0X1Cxg5iuxI.gB9Ke9bI9haeDK",
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				id: 2,
+				name: "Andre",
+				email: "andre@gmail.com",
+				password: "$2b$10$7hbN2PKBlaHUyovbm0fvL.GCG/y0X1Cxg5iuxI.gB9Ke9bI9haeDK",
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+		]);
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+	},
+};
