05a1a26506887781f609c2078b06c7eaf31ce59d
Tiyas <tiyas.akbar@gmail.com>
2021-02-25 04:43:59 +0700

    coba

--- seeders/1-author.js
+++ seeders/1-author.js
@@ -4,14 +4,14 @@ module.exports = {
 	up: async (queryInterface, Sequelize) => {
 		return queryInterface.bulkInsert("authors", [
 			{
-				name: "Tiyas",
+				name: "Tiyas Akbar",
 				email: "tiyas@gmail.com",
 				password: "$2b$10$7hbN2PKBlaHUyovbm0fvL.GCG/y0X1Cxg5iuxI.gB9Ke9bI9haeDK",
 				createdAt: new Date(),
 				updatedAt: new Date(),
 			},
 			{
-				name: "Andre",
+				name: "Andre Siga",
 				email: "andre@gmail.com",
 				password: "$2b$10$7hbN2PKBlaHUyovbm0fvL.GCG/y0X1Cxg5iuxI.gB9Ke9bI9haeDK",
 				createdAt: new Date(),
